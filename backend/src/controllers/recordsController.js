8073b1bacab31a7fe5c69b26c457155497ae0b91
voldin912 <takanamiharuki@gmail.com>
2025-06-13 03:25:28 +0900

    updated

--- backend/src/controllers/recordsController.js
+++ backend/src/controllers/recordsController.js
@@ -8,6 +8,50 @@ import PDFDocument from 'pdfkit';
 import archiver from 'archiver';
 import path from 'path';
 
+
+// import path from 'path'
+// import { nodewhisper } from 'nodejs-whisper'
+
+// // Need to provide exact path to your audio file.
+// const filePath = path.resolve(__dirname, 'YourAudioFileName')
+
+// await nodewhisper(filePath, {
+// 	modelName: 'base.en', //Downloaded models name
+// 	autoDownloadModelName: 'base.en', // (optional) auto download a model if model is not present
+// 	removeWavFileAfterTranscription: false, // (optional) remove wav file once transcribed
+// 	withCuda: false, // (optional) use cuda for faster processing
+// 	logger: console, // (optional) Logging instance, defaults to console
+// 	whisperOptions: {
+// 		outputInCsv: false, // get output result in csv file
+// 		outputInJson: false, // get output result in json file
+// 		outputInJsonFull: false, // get output result in json file including more information
+// 		outputInLrc: false, // get output result in lrc file
+// 		outputInSrt: true, // get output result in srt file
+// 		outputInText: false, // get output result in txt file
+// 		outputInVtt: false, // get output result in vtt file
+// 		outputInWords: false, // get output result in wts file for karaoke
+// 		translateToEnglish: false, // translate from source language to english
+// 		wordTimestamps: false, // word-level timestamps
+// 		timestamps_length: 20, // amount of dialogue per timestamp pair
+// 		splitOnWord: true, // split on word rather than on token
+// 	},
+// })
+
+// // Model list
+// const MODELS_LIST = [
+// 	'tiny',
+// 	'tiny.en',
+// 	'base',
+// 	'base.en',
+// 	'small',
+// 	'small.en',
+// 	'medium',
+// 	'medium.en',
+// 	'large-v1',
+// 	'large',
+// 	'large-v3-turbo',
+// ]
+
 // Transcription Queue Manager
 class TranscriptionQueue {
   constructor() {
